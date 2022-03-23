5cb467561d4ff973e1b3e609e79f44f28e2c404f
One <tradgardsmastaren@protonmail.com>
2022-03-23 11:32:48 +0100

    Initilized project

---
+++ src/test/java/com/grupp3/fakestagram/FakestagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp3.fakestagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakestagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
