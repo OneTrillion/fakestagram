10711dc1f9eed19e3ab5438d5eedf3526079ad38
xLara <xlaraa@hotmail.se>
2022-04-04 12:33:43 +0200

    sorterade och markerade alla metoder som funkar för posts

--- src/main/java/com/grupp3/fakestagram/api/PostsController.java
+++ src/main/java/com/grupp3/fakestagram/api/PostsController.java
@@ -5,6 +5,8 @@ import com.grupp3.fakestagram.service.PostService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.websocket.server.PathParam;
 import java.util.List;
 
@@ -16,25 +18,38 @@ public class PostsController {
 
 
     @PostMapping("/make-post") //funkar
-    public void makePost(@RequestBody Post post){
+    public void makePost( @Valid @RequestBody Post post){
         postService.makePost(post);
 
     }
 
-    @GetMapping("/view")
+    @GetMapping("/view-posts") //funkar
     public List<Post> showAllPosts(){
         return postService.viewAllPosts();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/post-{id}")//funkar
     public Post showPostById(@PathVariable Long id){
         return postService.findPostById(id);
     }
 
-    @GetMapping("/testpost")
-    public Post viewPosts(){
-        Post testPost = new Post("Här är första inlägget",23,"2022-03-28");
-        return testPost;
+    @DeleteMapping("/delete/{id}") //funkar
+    public void deletePostById(@PathVariable Long id){
+       postService.deletePostById(id);
+    }
+
+    @PutMapping("update/post/{id}")
+    public void updatePostById(){
+
+    }
+
+    @PutMapping("add/like")
+    public void likePost(){
+
+    }
+    @PutMapping("add/unlike")
+    public void unlikePost(){
+
     }
 
 }
