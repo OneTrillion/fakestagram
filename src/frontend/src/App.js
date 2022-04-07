046566d134222521cbe9538bd4852f8f26401b6b
One <tradgardsmastaren@protonmail.com>
2022-04-07 23:20:14 +0200

    Fixed so that we can upload images to the cloud and get an url response

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import {getAllUsers} from "./client";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 function App() {
 
+    const [image, setImage] = useState('');
+    const [loading, setLoading] = useState(false);
+/*
 
     //Test för hur man kan använda vår api VVV
     const [users, setUsers] = useState([{}]);
@@ -27,11 +29,40 @@ function App() {
         )
     //Test för hur vi kan använda vår api ^^^
 
+*/
+    //Lägger upp en ny bild på clouden och visar den på sidan
+    const uploadImage = async e => {
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'fakestagram')
+        setLoading(true);
+        const res = await fetch('https://api.cloudinary.com/v1_1/dg1uypbkk/image/upload',
+            {
+                method: 'POST',
+                body: data
+            })
+        const file = await res.json();
 
+        setImage(file.secure_url);
+        setLoading(false);
+
+    }
   return (
     <div className="App">
         <h1>Hello</h1>
-        {mapAllUsers}
+        <h1>Upload image</h1>
+        <input
+            type={"file"}
+            name={"file"}
+            placeholder={"Upload an image"}
+            onChange={uploadImage}
+        />
+        { loading ? (
+            <h3>Loading...</h3>
+        ): (
+            <img src={image} style={{width: '300px'}} alt={"asdasd"}/>
+        )}
     </div>
   );
 }
